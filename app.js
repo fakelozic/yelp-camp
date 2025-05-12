cbd1bdc0edf53b131ec31fba27a8ed71869831a3
Akash <zenakiyo@gmail.com>
2025-05-12 23:56:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+require("dotenv").config();
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("Database connected");
+}
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index.ejs", { campgrounds });
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = Campground(req.body.campground);
+  console.log(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new.ejs");
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const id = req.params.id;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/details.ejs", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit.ejs", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const updatedCampground = req.body;
+  console.log(updatedCampground);
+  const campground = await Campground.findByIdAndUpdate(req.params.id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const id = req.params.id;
+  const campground = await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening port ${process.env.PORT}`);
+});
---
+++ seeds/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const Campground = require("../models/campground");
+const { descriptors, places } = require("./seedHelpers");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("Database connected");
+}
+
+const sample = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      price: Math.floor(Math.random() * 20) + 20,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "What is Camping? Camping is an outdoor activity that involves staying the night/more than one night in a protective shelter out in nature. Camping is a broad term but in its essence, camping is a way of getting away from the hassle of urban life, to a more natural environment for a limited time.",
+    });
+    await camp.save();
+  }
+};
+seedDB().then(() => {
+  mongoose.connection.close();
+});
